aae0f5c57b51679ab2d6ac0d6088be4966d0525e
“Alexandrie” <alexandrie.be@gmail.com>
2022-04-26 17:38:48 +0200

    Burger menu - WIP

--- src/components/ComponentsSVG/BurgerIcon.jsx
+++ src/components/ComponentsSVG/BurgerIcon.jsx
@@ -1,15 +1,37 @@
 const BurgerIcon = () => {
   return (
     <svg
+      className='burger-icon'
       width='22'
       height='20'
       viewBox='0 0 22 20'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <rect x='1' y='16' width='22' height='1' fill='#0E184D' />
-      <rect x='1' y='9' width='22' height='1' fill='#0E184D' />
-      <rect x='1' y='2' width='22' height='1' fill='#0E184D' />
+      <rect
+        className='burger-icon--top'
+        x='1'
+        y='16'
+        width='22'
+        height='1'
+        fill='#0E184D'
+      />
+      <rect
+        className='burger-icon--middle'
+        x='1'
+        y='9'
+        width='22'
+        height='1'
+        fill='#0E184D'
+      />
+      <rect
+        className='burger-icon--bottom'
+        x='1'
+        y='2'
+        width='22'
+        height='1'
+        fill='#0E184D'
+      />
     </svg>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import BurgerIcon from './ComponentsSVG/BurgerIcon';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const itemsNavBar = ['about', 'shop', 'gallery', 'contact'];
+
+  const openNavToggle = () => {
+    setIsOpen(!isOpen);
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  };
+
   return (
-    <>
-      <div className='navbar__burger'>
+    <div className='navbar'>
+      <div className='navbar__burger' onClick={openNavToggle}>
         <BurgerIcon />
       </div>
 
-      <ul className='navbar__classic'>
+      <ul
+        className={`navbar__classic navbar__classic${
+          isOpen ? '--is-open' : ''
+        }`}
+      >
         {itemsNavBar.map((item) => (
           <li key={item} className='navbar__classic-item'>
             <Link to={`/${item}`}>{item}</Link>
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
