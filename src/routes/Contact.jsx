8d0996ff577033c8c3e5522b27d42cf29820e7ec
“Alexandrie” <alexandrie.be@gmail.com>
2022-04-27 17:11:49 +0200

    Contact component, style and responsive done - Gallery component WIP

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import Shop from '../assets/shop.png';
 
-const Contact = () => <div className='home'>Contact</div>;
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <p className='contact__title'>Atelier</p>
+      <div className='contact__address'>
+        <p className='contact__title--city'>73 rue du Poteau, Paris 18</p>
+        <p className='contact__title--email'>m : hello@atelierkimle.com</p>
+      </div>
+      <img
+        src={Shop}
+        className='contact__shop'
+        alt='portrait'
+        draggable='false'
+      />
+    </div>
+  );
+};
 
 export default Contact;
