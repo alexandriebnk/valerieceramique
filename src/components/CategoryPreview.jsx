1716ed67cbdc08f2f8decb6c976e5b93e9163ebe
“Alexandrie” <alexandrie.be@gmail.com>
2022-05-04 18:26:09 +0200

    CategoryPreview component - WIP

--- src/components/CategoryPreview.jsx
+++ src/components/CategoryPreview.jsx
@@ -3,7 +3,26 @@ import Category from './Category';
 
 const CategoryPreview = () => {
   return (
-    <div className='category-preview'>
+    <div className='preview'>
+      <div className='preview__title'>
+        <h1>Category</h1>
+        <p>See all</p>
+      </div>
+      <div className='preview__category'>
+        <div className='preview__category--details'>
+          <p className='preview__category--details-brand'>Valérie Céramique</p>
+          <div className='preview__category--details-image'>
+            <img
+              src='/gallery/gallery_1.png'
+              className='preview__category--details-image-item'
+              alt='product'
+              draggable='false'
+            />
+          </div>
+          <div className='preview__category--details-title'></div>
+        </div>
+      </div>
+
       <Category />
     </div>
   );
