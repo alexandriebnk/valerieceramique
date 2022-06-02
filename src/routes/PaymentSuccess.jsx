89ce179386d4fcc8df6c735bd498bb848869e6c9
“Alexandrie” <alexandrie.be@gmail.com>
2022-06-02 12:54:43 +0200

    Remove createOrder from PaymentSuccess component

--- src/routes/PaymentSuccess.jsx
+++ src/routes/PaymentSuccess.jsx
@@ -1,35 +1,7 @@
-import React, { useEffect } from 'react';
-import { gql, useMutation } from '@apollo/client';
+import React from 'react';
 import UserMessage from '../components/UserMessage';
-import Loader from '../components/Loader';
-
-const CREATEORDER = gql`
-  mutation CreateOrder($address: String!) {
-    createOrder(data: { address: $address }) {
-      data {
-        id
-        attributes {
-          address
-        }
-      }
-    }
-  }
-`;
 
 const PaymentSuccess = () => {
-  const [mutateFunction, { data, loading, error }] = useMutation(CREATEORDER, {
-    variables: { address: 'super order' },
-  });
-
-  useEffect(() => {
-    //mutateFunction();
-
-    if (data) console.log(data);
-  }, [data]);
-
-  if (loading) return <Loader />;
-  if (error) return <p>Error..</p>;
-
   return (
     <div>
       <UserMessage
